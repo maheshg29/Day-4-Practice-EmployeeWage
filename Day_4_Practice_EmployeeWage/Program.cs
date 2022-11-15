af347b850bcb979fcae6b405c0c941220ccac7bf
mahesh <gangurdemahesh9@gmail.com>
2022-11-15 16:14:07 +0530

    [Mahesh] add UC1 file

--- Day_4_Practice_EmployeeWage/Program.cs
+++ Day_4_Practice_EmployeeWage/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Day_4_Practice_EmployeeWage
 {
-   class Program
+    class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Not Present");
+            }
+
         }
     }
 }
